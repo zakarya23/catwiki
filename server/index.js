324f4bb6a6da4771211373a0567bfb4e0c94de6a
Zakarya Ahmed Butt <zakarya.butt@student.unimelb.edu.au>
2022-02-23 15:11:53 +0500

    ready for heokru

--- server/index.js
+++ server/index.js
@@ -11,16 +11,24 @@ const __dirname = path.resolve()
 
 app.use('/api/cats', catRouter)
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, '../client/build')))
+// app.use(express.static(path.resolve(__dirname, '../client/build')))
 
-app.get('/api', (req, res) => {
-	res.json({ message: 'Hello from CatWiki!' })
-})
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '/client/build')))
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	})
+} else {
+	app.get('/', (req, res) => {
+		res.send('Hello World!')
+	})
+}
 
 // All other GET requests not handled before will return our React app
-app.get('*', (req, res) => {
-	res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
-})
+// app.get('*', (req, res) => {
+// 	res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
+// })
 
 app.listen(PORT, () => {
 	console.log(`Server listening on ${PORT}`)
