1c723e2a5b835d4b18b5db56134eae50f9a7f1e2
Zakarya Ahmed Butt <zakarya.butt@student.unimelb.edu.au>
2022-02-23 13:45:21 +0500

    Initial pages & routes set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,31 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react'
+import { Routes, Route, BrowserRouter as Router } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
+import SearchScreen from './screens/SearchScreen'
 
 function App() {
-  const [data, setData] = React.useState(null);
+	const [data, setData] = React.useState(null)
 
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
+	React.useEffect(() => {
+		fetch('/api')
+			.then((res) => res.json())
+			.then((data) => setData(data.message))
+	}, [])
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<Header />
+			<main>
+				<Routes>
+					<Route path='/' element={<HomeScreen />} />
+					<Route path='/search/:keyword' element={<SearchScreen />} />
+				</Routes>
+			</main>
+			<Footer />
+		</Router>
+	)
 }
 
-export default App;
+export default App
